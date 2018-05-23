60ca73d728b18132760e148f78ec63ddd91a47fd
Magnum Polmatier <maganum@hotmail.com>
2018-05-23 02:05:17 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"]
+
+function addElementToBeginningOfArray() {
+var moreChocolateBars = [(), ...chocolateBars]
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+chocolateBars.unpush()
+}
+
